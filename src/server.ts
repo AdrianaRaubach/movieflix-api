e51a79afcd5796eb31dd89880729ea3085a3365f
AdrianaRaubach <raubach.adrianadw@gmail.com>
2024-05-26 11:51:16 -0300

    adicionada a remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -23,57 +23,76 @@ app.get("/movies", async (req, res) => {
 app.post("/movies", async (req, res) => {
 
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
-    
-    try{
+
+    try {
 
         const movieWithSameTitle = await prisma.movie.findFirst({
             where: { title: { equals: title, mode: "insensitive" } },
         });
 
-        if(movieWithSameTitle) {
-            return res.status(409).send({ message: "Já existe um filme com este título"});
+        if (movieWithSameTitle) {
+            return res.status(409).send({ message: "Já existe um filme com este título" });
         }
 
-    await prisma.movie.create({
-        data: {
-            title: title,
-            genre_id: genre_id,
-            language_id: language_id,
-            oscar_count: oscar_count,
-            release_date: new Date(release_date),
-        }
-    });
-    }catch(error){
-        return res.status(500).send({message: "Falha ao cadastrar um filme"})
-    }    
+        await prisma.movie.create({
+            data: {
+                title: title,
+                genre_id: genre_id,
+                language_id: language_id,
+                oscar_count: oscar_count,
+                release_date: new Date(release_date),
+            }
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" })
+    }
     res.status(201).send();
 });
 
 app.put("/movies/:id", async (req, res) => {
     const id = Number(req.params.id);
-try{
-    const movie = await prisma.movie.findUnique({
-        where:{
-            id
-        }
-    });
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id
+            }
+        });
 
-    if(!movie){
-        return res.status(404).send({ message: "Filme não encontrado" });
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        data.release_date = data.release_date ? new Date(data.release_date) : undefined;
+
+        await prisma.movie.update({
+            where: {
+                id
+            },
+            data: data
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar o registro do filme" });
+    }
+    res.status(200).send();
+});
+
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
+
+        if (!movie) {
+            return res.status(404).send({ message: "O filme não foi encontrado" });
+        }
+
+        await prisma.movie.delete({ where: { id } });
+
+    } catch (error) {
+        return res.status(500).send({ message: "Não foi possível remover o filme" });
     }
 
-    const data = { ...req.body};
-    data.release_date = data.release_date ? new Date(data.release_date) : undefined;
-    
-    await prisma.movie.update({
-        where: {
-            id
-        },
-        data: data
-    });
-}catch(error){
-    return res.status(500).send({message: "Falha ao atualizar o registro do filme" });
-}
     res.status(200).send();
 });
 
